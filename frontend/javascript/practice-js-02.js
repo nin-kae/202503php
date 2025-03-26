78c388ec3eca2e3de31c99975a7c5d7977f3054d
nin-kae <renhuaying0401@gmail.com>
2025-03-26 21:28:52 +0900

    update

--- frontend/javascript/practice-js-02.js
+++ frontend/javascript/practice-js-02.js
@@ -80,3 +80,75 @@ let = [ 'iphone', '三星', '小米' ];
 for ( let key in userInfo ) {
     console.log ( key + ': ' + userInfo[key] );
 }
+
+console.log ( '----------- for of 循环 -----------' ); 
+
+console.log ( '----------- break -----------' );
+
+for(let i = 0; i<= 10; i++) {
+    if ( i === 3 ){
+        break;
+    }
+    console.log (' 当前值为：' + i );
+}
+
+console.log ( '----------- continue -----------' );
+
+for(let i = 0; i<= 10; i++) {
+    if ( i === 3 ){
+        continue;
+    }
+    console.log (' 当前值为：' + i );
+}
+
+console.log ( '----------- 函数 -----------' );
+
+// 函数声明
+function add( a, b ){
+    return a + b;
+}
+let sum = add ( a = 10, b = 20 )
+console.log (sum);
+
+
+//函数表达式
+let totalPrice = function (a,b){
+    return price * count;
+}
+console.log ( totalPrice( price = 5,count = 2 ) );
+
+//箭头函数
+let divide = (  a,b ) => a / b;
+console.log(divide(10,2));
+
+console.log ( '----------- 函数的参数 -----------' );
+
+function sayHello( name = '张三'){
+    console.log('你好，' + name);
+}
+sayHello('李四');
+sayHello();
+
+function sayHi(name, age, callback){
+    console.log('你好,' + name + ',今年' + age + '岁');
+    callback();
+}
+
+sayHi('王五', 25, function() {
+    console.log('这是回调函数');
+})
+
+console.log ( '----------- 参数的解构赋值 -----------' );
+
+function printInfo({name, age}){
+    console.log('姓名' + name + '，年龄' + age);
+}
+let user = {}
+
+function sayGoodbye(){
+    //return
+}
+
+let Goodbye=sayGoodbye
+
+console.log ( '----------- 参数的解构赋值 -----------' );
