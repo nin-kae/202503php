3a079d18dbca14d6ba0f6741575d8e7be0e7829a
nin-kae <renhuaying0401@gmail.com>
2025-03-28 22:58:34 +0900

    Update practice-tools-03.js

--- frontend/javascript/practice-tools-03.js
+++ frontend/javascript/practice-tools-03.js
@@ -28,3 +28,52 @@ reset.addEventListener ( 'click', () => {
     timer.innerText = count;
 });
 //计时器结束
+
+// 计算 BMI 开始
+function calculateBMI(){
+    let weight = parseFloat ( document.getElementById ( 'weight' ).value );
+    let height = parseFloat ( document.getElementById ( 'height' ).value );
+
+
+    if ( isNaN (weight) || weight <= 2.5) {
+        alert ( '请输入有效体重' );
+    return;
+    }
+
+    if ( isNaN (height) || height <= 50 ) {
+        alert ( '请输入有效身高' );
+        return;
+    }
+
+    // 计算 BMI
+    let bmi = weight / ( height * height ) * 10000;
+    bmi = bmi.toFixed (2);
+    let resultElement = document.getElementById ( 'bmi-result' );
+
+    let innerHTML = '您的 BMI 值是：' + bmi;
+
+    // 计算结果resul
+    if (bmi < 18.5) {
+        resultElement.innerHTML = innerHTML + ' 体重过轻';
+    } else if ( bmi < 24 ) {
+        resultElement.innerHTML = innerHTML + ' 体重正常';
+    } else if ( bmi < 28 ) {
+        resultElement.innerHTML = innerHTML + ' 体重过重';
+    } else if ( bmi < 35 ) {
+        resultElement.innerHTML = innerHTML + ' 体重肥胖';
+    } else {
+        resultElement.innerHTML = innerHTML + ' ';
+    }
+}
+
+// 阻止表单提交的默认行为 比如 a元素中 点击后跳转页面；form 提交后刷新页面或者跳转；button 提交表单并刷新页面
+document.getElementById ( 'bmi-form' ).onsubmit = function ( event ) {
+    event.preventDefault ();
+};
+
+//监听按钮点击事件
+document.getElementById ( 'btn-calculate' ).onclick = function () {
+    calculateBMI ();
+};
+
+// 计算 BMI 结束
