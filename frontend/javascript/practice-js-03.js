8a78ea5315940c5b4fd35d91080edac55e100ef7
nin-kae <renhuaying0401@gmail.com>
2025-03-28 19:16:42 +0900

    Update practice-js-03.js

--- frontend/javascript/practice-js-03.js
+++ frontend/javascript/practice-js-03.js
@@ -9,16 +9,37 @@ document.addEventListener ( 'keydown', function ( event ) {
 document.addEventListener ( 'submit', function ( event ) {
     event.preventDefault(); //阻止表单默认提交行为
 
-    let form = event.target;// 获取表单元素
+    let form = event.target; // 获取表单元素
     let formData = new FormData(form);
 
     console.log ( formData.entries () );
 
     let data = {}; //创建一个空对象，用于存放表单数据
-    for (let [ key, value ] of formData.entries()) {
+    for (let [ key, value ] of formData.entries () ) {
         data [ key ] = value;
     }
 
     document.getElementById( 'submit-event-output' ). innerHTML = JSON.stringify (data);
 
+});
+
+// 移除事件监听
+function handleClick(event) {
+    alert('按钮被点击了');
+}
+
+let button = document.getElementById ( 'remove-event-listener' );
+button.addEventListener ( 'click', handleClick );
+button.removeEventListener ( 'click', handleClick );
+
+let parent = document.getElementById ( 'parent' );
+let child = document.getElementById ( 'child' );
+
+parent.addEventListener ( 'click', function () {
+    alert ( '父级元素被点击了' );
+});
+
+child.addEventListener ( 'click', function (event) {
+    event.stopPropagation (); //阻止事件弹窗
+    alert ( '子元素被点击了' )
 });
