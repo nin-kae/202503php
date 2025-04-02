1a830829d0d582a2673448f0b990a5314cf11fd4
nin-kae <renhuaying0401@gmail.com>
2025-04-02 19:21:35 +0900

    update

--- frontend/javascript/jquery/Raffle-feature.js
+++ frontend/javascript/jquery/Raffle-feature.js
@@ -3,12 +3,12 @@ var lottery = {
     count: 0,
     timer: 0,
     speed: 20,
-    tomes: 0,
+    times: 0,
     cycle: 50,
     price: -1,
     init:function (id) {
-        if ( $( "#" + id ).find ( 'lottery-unit' ).length > 0 ) {
-            $lottery - $ ( "#" + id );
+        if ( $("#" + id).find('lottery-unit').length > 0 ) {
+            $lottery = $ ( "#" + id );
             $units = $lottery.find ( '.lottery-unit' );
             this.obj = $lottery;
             this.count = $units.length;
@@ -19,17 +19,20 @@ var lottery = {
         var index = this.index;
         var count = this.count;
         var lottery = this.obj;
+
         $(lottery).find ( '.lottery-unit.lottery-unit-' + index ).removeClass ( 'active' );
+        
         index += 1;
         if ( index > count -1 ) {
             index = 0;
         };
+
         $(lottery).find ( '.lottery-unit.lottery-unit-' + index).addClass ( 'active' );
         this.index = index;
         return false;
     },
     stop:function ( index ) {
         this.prize = index;
-        return false
+        return false;
     },
 };
