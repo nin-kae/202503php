9ce162a89249dc7e28b019f3fde1bb2dbb610244
nin-kae <renhuaying0401@gmail.com>
2025-05-09 19:08:31 +0900

    Update index.blade.php

--- php/laravel/resources/views/posts/index.blade.php
+++ php/laravel/resources/views/posts/index.blade.php
@@ -8,15 +8,47 @@
         <div class="w-full sm:w-auto flex flex-col sm:flex-row gap-2">
             <form action="{{ route('posts.index') }}" method="GET" class="flex-grow sm:flex-grow-0">
                 <div class="relative">
-                    <input type="search" name="search" placeholder="{{ __('Search posts...') }}" value="{{ request('search') }}"
-                           class="block w-full pl-10 pr-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md leading-5 bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:placeholder-gray-400 dark:focus:placeholder-gray-500 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <svg class="h-5 w-5 text-gray-400 dark:text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                    {{-- 搜索输入框，左侧不用留 padding 了，右侧给两边留出位置 --}}
+                    <input
+                        type="search"
+                        id="search"
+                        name="search"
+                        placeholder="{{ __('Search posts...') }}"
+                        value="{{ request('search') }}"
+                        autocomplete="off"
+                        class="block w-full pl-3 pr-12 py-2 border border-gray-300 rounded-md text-gray-900 placeholder-gray-500
+             focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500
+             sm:text-sm"/>
+
+                    {{-- 清除按钮 X --}}
+                    <button
+                        type="button"
+                        onclick="document.getElementById('search').value='';"
+                        aria-label="Clear search"
+                        class="absolute inset-y-0 right-10 flex items-center px-2
+             text-gray-400 hover:text-gray-600 focus:outline-none">
+                        {{-- 这里直接内联一个灰色 X svg --}}
+                        <svg class="h-5 w-5 $gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M6 18L18 6M6 6l12 12" clip-rule="evenodd"/>
                         </svg>
-                    </div>
+                    </button>
+
+                    {{-- 提交按钮（右侧放大镜） --}}
+                    <button
+                        type="submit"
+                        aria-label="Search"
+                        class="absolute inset-y-0 right-2 flex items-center px-2
+             text-gray-400 hover:text-gray-800 focus:outline-none"
+                    >
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg"
+                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M21 21l-4.35-4.35M17 10a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                        </svg>
+                    </button>
                 </div>
             </form>
+
             <a href="{{ route('posts.create') }}"
                class="w-full sm:w-auto px-4 py-2 text-sm font-medium text-white text-center bg-indigo-600 hover:bg-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                 {{ __('Create New Post') }}
